f336212cd3e2fddbc00c28ded1a98118401e7e3b
PelegZur <57677867+PelegZur@users.noreply.github.com>
2023-03-30 12:27:19 +0300

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,51 +1,24 @@
 resource "aws_s3_bucket" "data" {
-  
-  
   # bucket is public
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
-  acl           = "public-read"
   force_destroy = true
   tags = merge({
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
     }, {
-    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
+    git_commit           = "4d57f83ca4d3a78a44fb36d1dcf0d23983fa44f5"
     git_file             = "terraform/aws/s3.tf"
-    git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "nimrodkor@gmail.com"
-    git_modifiers        = "nimrodkor"
+    git_last_modified_at = "2022-05-18 07:08:06"
+    git_last_modified_by = "nimrod@bridgecrew.io"
+    git_modifiers        = "34870196+LironElbaz/nimrod/nimrodkor"
     git_org              = "bridgecrewio"
     git_repo             = "terragoat"
     yor_trace            = "0874007d-903a-4b4c-945f-c9c233e13243"
   })
 }
-
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "data" {
-  bucket = aws_s3_bucket.data.bucket
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
-
-
-resource "aws_s3_bucket" "data_log_bucket" {
-  bucket = "data-log-bucket"
-}
-
-resource "aws_s3_bucket_logging" "data" {
-  bucket = aws_s3_bucket.data.id
-
-  target_bucket = aws_s3_bucket.data_log_bucket.id
-  target_prefix = "log/"
-}
-
 
 resource "aws_s3_bucket_object" "data_object" {
   bucket = aws_s3_bucket.data.id
@@ -111,7 +84,6 @@ resource "aws_s3_bucket" "operations" {
     git_repo             = "terragoat"
     yor_trace            = "29efcf7b-22a8-4bd6-8e14-1f55b3a2d743"
   })
-
 }
 
 resource "aws_s3_bucket" "data_science" {
