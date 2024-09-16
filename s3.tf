7bdaa213b42fda4189011f9e4e9ac5e7128e1a09
Tomer Eskenazi <tomeresk@gmail.com>
2024-09-16 11:37:19 +0300

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -3,6 +3,6 @@ resource "aws_s3_bucket" "data" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
-  bucket        = "prefix-data333"
+  bucket        = "prefix-data33333"
   force_destroy = true
 }
