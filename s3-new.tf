460c997d25f3336585f88cb9ff284983ab55e66f
Tomer Eskenazi <tomeresk@gmail.com>
2025-05-27 18:57:30 +0300

    new bucket

---
+++ s3-new.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "prefix-data-new"
+  force_destroy = true
+}
