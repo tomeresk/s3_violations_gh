c6f6d5d9009eaf8004627ae7ad34f377060fa804
Liran Korn <e.shfiyut@gmail.com>
2025-06-05 19:32:28 +0300

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -3,6 +3,7 @@ resource "aws_s3_bucket" "data" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
+  # one more try
   bucket        = "prefix-data"
   force_destroy = true
 }
